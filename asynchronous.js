e235b9acbab5607def70790730703f799f4d1ddb
Sohel Shaikh <sohel@simformsolutions.com>
2023-03-06 18:06:00 +0530

    initial commit

---
+++ asynchronous.js
@@ -0,0 +1,11 @@
+
+function x() {
+    console.log("This is x")
+}
+function y() {
+    console.log("This is y");
+}
+x(); y();
+setTimeout(() => {
+    console.log("Hi this is sohel shaikh")
+}, 5000);
---
+++ event.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById('btn').addEventListener('click',function(){
+    // console.log("hi SOhel");
+
+    document.getElementById('demo1').innerHTML=Date();
+})
